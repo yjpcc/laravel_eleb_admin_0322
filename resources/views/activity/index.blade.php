d4d9d3e020730c9260156e631908b7647d1c42bb
yjpcc <1104429862@qq.com>
2018-07-25 09:49:59 +0800

    优化活动管理

--- resources/views/activity/index.blade.php
+++ resources/views/activity/index.blade.php
@@ -1,26 +1,30 @@
 @extends('default')
 @section('content')
     <div class="row">
-        <div class="col-lg-10">
+        <div class="col-lg-8">
             <ol class="breadcrumb">
                 <li><a href="">Home</a></li>
                 <li>Article</li>
             </ol>
         </div><!-- /.col-lg-6 -->
-        <div class="col-lg-2">
-            <form action="{{ route('activitys.index') }}" method="get">
-                <div class="input-group">
-                    <select class="form-control" name="status">
-                            <option value="0" {{ $status==0?'selected':'' }}>全部</option>
-                            <option value="1" {{ $status==1?'selected':'' }}>未开始</option>
-                            <option value="2" {{ $status==2?'selected':'' }}>进行中</option>
-                            <option value="3" {{ $status==3?'selected':'' }}>已结束</option>
-                    </select>
-                    <span class="input-group-btn">
-        <button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search"></span></button>
-                </span>
-                </div>
-            </form>
+        <div class="col-lg-4">
+            <a href="{{ route('activitys.index',['status'=>0]) }}" class="btn btn-default">全部</a>
+            <a href="{{ route('activitys.index',['status'=>1]) }}" class="btn btn-info">未开始</a>
+            <a href="{{ route('activitys.index',['status'=>2]) }}" class="btn btn-success">进行中</a>
+            <a href="{{ route('activitys.index',['status'=>3]) }}" class="btn btn-danger">已结束</a>
+            {{--<form action="{{ route('activitys.index') }}" method="get">--}}
+                {{--<div class="input-group">--}}
+                    {{--<select class="form-control" name="status">--}}
+                            {{--<option value="0" {{ $status==0?'selected':'' }}>全部</option>--}}
+                            {{--<option value="1" {{ $status==1?'selected':'' }}>未开始</option>--}}
+                            {{--<option value="2" {{ $status==2?'selected':'' }}>进行中</option>--}}
+                            {{--<option value="3" {{ $status==3?'selected':'' }}>已结束</option>--}}
+                    {{--</select>--}}
+                    {{--<span class="input-group-btn">--}}
+        {{--<button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search"></span></button>--}}
+                {{--</span>--}}
+                {{--</div>--}}
+            {{--</form>--}}
         </div>
     </div>
     <table class="table table-striped table-hover table-bordered table-condensed">
